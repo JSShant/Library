80fcd4c4df5718f45fbc7771cbe98ae8dc0dfbc6
JSShant <joshua.shanthikumar@outlook.com>
2022-07-25 03:54:11 +1000

    Using react router to dynamically change pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
-import Featured from "./components/Featured";
 import Footer from "./components/Footer";
-import Highlights from "./components/Highlights";
-import Landing from "./components/Landing";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Books from "./pages/Books";
 import { books } from "./data.js";
+import BookInfo from "./pages/BooksInfo.jsx";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
         <Route exact path="/" component={Home} />
-        <Route path="/books" render={() => <Books books={books} />} />
+        <Route exact path="/books" render={() => <Books books={books} />} />
+        <Route path="/books/:id" render={() => <BookInfo books={books} />} />
         <Footer />
       </div>
     </Router>

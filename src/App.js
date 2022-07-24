e5b2cbc65e22afe05966ef29112f6a4755d5f98f
JSShant <joshua.shanthikumar@outlook.com>
2022-07-24 22:00:48 +1000

    Created Home Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Discounted from "./components/Discounted";
+import Explore from "./components/Explore";
+import Featured from "./components/Featured";
+import Footer from "./components/Footer";
+import Highlights from "./components/Highlights";
+import Landing from "./components/Landing";
+import Nav from "./components/Nav";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faShoppingCart,
+  faTimes,
+  faBolt,
+  faBookOpen,
+  faTags,
+  faStar,
+  faStarHalfAlt,
+} from "@fortawesome/free-solid-svg-icons";
+library.add(
+  faBars,
+  faShoppingCart,
+  faTimes,
+  faBolt,
+  faBookOpen,
+  faTags,
+  faStar,
+  faStarHalfAlt
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

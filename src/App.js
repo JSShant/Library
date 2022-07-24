e09b06e86e085e798f250e136ae2824949947864
JSShant <joshua.shanthikumar@outlook.com>
2022-07-25 00:55:25 +1000

    Created Books page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Footer from "./components/Footer";
 import Highlights from "./components/Highlights";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Books from "./pages/Books";
+import { books } from "./data.js";
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route path="/books" render={() => <Books books={books} />} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
